e240c22c03737f8497ff9069967a25dc5172abec
Marija Topalova <marijatopalova1@outlook.com>
2024-09-19 22:30:08 +0200

    Add project files.

---
+++ CatalogService/Controllers/CatalogController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCatalog()
+        {
+            return Ok(new { Message = "Catalog Service", Data = new[] { "Item1", "Item2" } });
+        }
+    }
+}
---
+++ OrderService/Controllers/OrderController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            return Ok(new { Message = "Order Service", Data = new[] { "Order1", "Order2" } });
+        }
+    }
+}
